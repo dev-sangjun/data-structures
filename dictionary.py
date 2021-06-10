b46f3791d5b1b8a266cc600bc67548f009455318
dev-sangjun <dev.sangjun@gmail.com>
2021-06-10 16:54:15 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -4,6 +4,16 @@ dict_["B"] = 2
 
 print(dict_["A"])
 
+# contains keys
+print("A" in dict_)
+
+# contains values
+print(1 in dict_.values())
+
+# contains key, value
+print("A" in dict_ and dict_["A"] == 1)
+
+# clear
 dict_.clear()
 print(dict_)
 
